76d305aa0f0fd6b599e51ef36fc6f8061177f178
Jim Wright <jim@legl.com>
2021-06-10 08:34:24 -0600

    Export containFocus helper

--- packages/overlays/index.js
+++ packages/overlays/index.js
@@ -8,3 +8,4 @@ export { ArrowMixin } from './src/ArrowMixin.js';
 export { withBottomSheetConfig } from './src/configurations/withBottomSheetConfig.js';
 export { withModalDialogConfig } from './src/configurations/withModalDialogConfig.js';
 export { withDropdownConfig } from './src/configurations/withDropdownConfig.js';
+export { containFocus } from './src/utils/contain-focus.js';
